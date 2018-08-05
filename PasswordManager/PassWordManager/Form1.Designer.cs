b000c1f813eda192ce0ec008bdd4f2d2bccd8a8e
BNDKG <zbsqqyx@qq.com>
2018-08-05 21:35:52 +0800

    密码管理首次提交

---
+++ PasswordManager/PassWordManager/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace PassWordManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(192, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 75);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "保存密码";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(192, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(198, 35);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.Text = "输入名称";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 75);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "读取密码";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 75);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "打开项目文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(412, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "位数";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(578, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(115, 137);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "包含的字符串";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(23, 101);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(48, 16);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "!@#$";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(23, 79);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(42, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "A~Z";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(23, 57);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(42, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "a~z";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(23, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "0~9";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(456, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(41, 21);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "16";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(78, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 50);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "输出密码";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 75);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "生成密码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 18F);
+            this.label2.Location = new System.Drawing.Point(80, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "保存名称";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 375);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PasswordCreater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PasswordManager/PassWordManager/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+//aes加密
+using System.Security.Cryptography;
+//序列化
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PassWordManager
+{
+    public partial class Form1 : Form
+    {
+        string OriPath;
+
+        string AES_KEY = "12345678876543211234567887654abc";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OriPath = System.IO.Directory.GetCurrentDirectory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int inputfigures = Convert.ToInt32(textBox2.Text);
+
+            bool[] range = { true, true, true, true };
+
+            range[0] = checkBox1.Checked;
+            range[1] = checkBox2.Checked;
+            range[2] = checkBox3.Checked;
+            range[3] = checkBox4.Checked;
+
+            //复制到显示
+            textBox1.Text = aes_logic.PasswordCreate(inputfigures, range);
+            //
+
+            //复制到剪贴板
+            Clipboard.SetText(textBox1.Text);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string path = OriPath;
+
+            System.Diagnostics.Process.Start("explorer.exe", path);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+
+            file.InitialDirectory = OriPath + "\\library";
+            file.ShowDialog();
+
+            string read = file.InitialDirectory + "\\" + file.SafeFileName;
+
+            PassWordStruct aaa = new PassWordStruct();
+
+            try
+            {
+                aaa = (PassWordStruct)aes_logic.LoadPassword(read);
+
+                textBox1.Text = aes_logic.AesDecrypt(aaa.password, AES_KEY);
+
+                textBox3.Text = aaa.name;
+                Clipboard.SetText(textBox1.Text);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("错误路径！！！");
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string SavePath = OriPath + "\\library\\" + textBox3.Text + ".pwd";
+
+            if (System.IO.File.Exists(SavePath))
+            {
+                //这里可以改成跳出是否覆盖
+                MessageBox.Show("该名称密码已存在");
+            }
+            else
+            {
+                string aespassword = aes_logic.AesEncrypt(textBox1.Text, AES_KEY);
+                PassWordStruct aaa = new PassWordStruct();
+                aaa.NO = 10;
+                aaa.name = textBox3.Text;
+                aaa.password = aespassword;
+
+                aes_logic.SavePassword(aaa, SavePath);
+            }
+
+        }
+
+    }
+
+    [Serializable]
+    public struct PassWordStruct  //自定义的数据类型。用来描述员工的信息。 
+    {
+        public int NO;
+        public string key;
+        public string name;
+        public string password;
+        public string info;
+
+        public PassWordStruct(int type)
+        {
+            NO = 0;
+            key = "";
+            name = "";
+            password = "";
+            info = "";
+        }
+
+    }
+    public class aes_logic
+    {
+
+
+
+        /// <summary>
+        ///  AES 加密
+        /// </summary>
+        /// <param name="str">明文（待加密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesEncrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            Byte[] toEncryptArray = Encoding.UTF8.GetBytes(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateEncryptor();
+            Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        /// <summary>
+        ///  AES 解密
+        /// </summary>
+        /// <param name="str">明文（待解密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesDecrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            Byte[] toEncryptArray = Convert.FromBase64String(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateDecryptor();
+            Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+        public static void SavePassword(object password, string filename) //序列化保存
+        {
+            try
+            {
+                FileStream fs = new FileStream(filename, FileMode.Create);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, password);
+                fs.Close();
+                MessageBox.Show("保存成功！");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("保存失败！！！");
+            }
+        }
+        public static object LoadPassword(string filename)            //序列化读取文件
+        {
+            object password = new object();
+            try
+            {
+                FileStream fs = new FileStream(filename, FileMode.Open);
+                BinaryFormatter bf = new BinaryFormatter();
+                password = bf.Deserialize(fs);
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("读取失败！！！");
+            }
+
+            return password;
+        }
+
+        public static string PasswordCreate(int figures, bool[] PasswordRange)
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            Random rnd2 = new Random(Guid.NewGuid().GetHashCode());
+
+            string[] arrays = new string[4];
+            arrays[0] = "0123456789";
+            arrays[1] = "abcdefghijklmnopqrstuvwxyz";
+            arrays[2] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            arrays[3] = "~!@#$%&*();/";
+
+            string array = "";
+            for (int i = 0; i < 4; i++)
+            {
+                if (PasswordRange[i])
+                {
+                    array += arrays[i];
+                }
+
+            }
+
+
+            int[] counter = new int[100];
+
+            int PasswordLength = figures;
+            string output = "";
+
+            for (int i = 0; i < PasswordLength; i++)
+            {
+
+                int nowindex;
+                //随机生成生成下一个随机数的次数
+                int creattimes = rnd.Next(0, 10);
+
+                while (creattimes > 0)
+                {
+                    nowindex = rnd.Next(0, array.Length);
+                    creattimes--;
+                }
+
+                nowindex = rnd.Next(0, array.Length);
+                counter[nowindex]++;
+                output += array[nowindex];
+
+
+            }
+
+            return output;
+        }
+
+    }
+
+
+}
